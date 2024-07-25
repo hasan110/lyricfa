615f2cad61b426c10a930212d487de6217e5963d
hasan110 <hasanhosseininum7@gmail.com>
2024-07-25 09:21:08 +0330

    add new musics list api

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -585,6 +585,8 @@ class MusicController extends Controller
             $music->singers = $singer;
             $music->num_like = $num_like;
             $music->num_comment = $num_comment;
+            $music->readable_like = $this->getReadableNumber(intval($num_like));
+            $music->readable_views = $this->getReadableNumber(intval($music->views));
 
             unset($music['id'], $music['name'], $music['persian_name'], $music['album'],
                 $music['degree'], $music['size'], $music['interest'],
