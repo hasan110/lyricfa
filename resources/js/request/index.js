1b6a578c2b96e379d0602e7f4e164262faf2f871
hasan110 <hasanhosseininum7@gmail.com>
2025-04-05 12:21:58 +0330

    move configuration data to env

--- resources/js/request/index.js
+++ resources/js/request/index.js
@@ -1,8 +1,9 @@
 import VueCookies from 'vue-cookies';
 import axios from 'axios';
+import config from '../config'
 
 const Axios = axios.create({
-    baseURL: 'http://lyricfa.local/',
+    baseURL: config.API_BASE_URL,
     // baseURL: 'https://panel.lyricfa.app/',
     headers: {'ApiToken': VueCookies.get('admin_token')}
 });
